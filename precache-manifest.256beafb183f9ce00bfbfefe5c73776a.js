5efa1a285acaef8cf321d2390f27d7e32f973f92
DmitriyKulikovskiy <59229226+DmitriyKulikovskiy@users.noreply.github.com>
2020-04-16 21:24:59 +0300

    Updates

---
+++ precache-manifest.256beafb183f9ce00bfbfefe5c73776a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73932e80344772826ee5616b5c46d50e",
+    "url": "/MyToDoList/index.html"
+  },
+  {
+    "revision": "66423dad9f39749936be",
+    "url": "/MyToDoList/static/css/main.1a78a4da.chunk.css"
+  },
+  {
+    "revision": "03cf1df3ed4d4b27b2d0",
+    "url": "/MyToDoList/static/js/2.d1b4e550.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/MyToDoList/static/js/2.d1b4e550.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66423dad9f39749936be",
+    "url": "/MyToDoList/static/js/main.6f3aa37f.chunk.js"
+  },
+  {
+    "revision": "ffbfe6089e4f33845bf3",
+    "url": "/MyToDoList/static/js/runtime-main.d0505a6a.js"
+  }
+]);
